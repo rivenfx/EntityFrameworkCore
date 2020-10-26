ed2144ef301eb7d9345138001382064252037dde
staneee <yi.hang@live.com>
2020-10-26 16:57:27 +0800

    修改编译脚本

--- nupkg/common.ps1
+++ nupkg/common.ps1
@@ -7,6 +7,7 @@ $packOutputFolder = Join-Path $packFolder "dist"        # 输出nuget package �
 
 # 所有的项目名称
 $projects = (
+    "Riven.EntityFrameworkCore.DevartOracle",
     "Riven.EntityFrameworkCore.Oracle",
-    "Riven.EntityFrameworkCore.PostgreSQL",
+    "Riven.EntityFrameworkCore.PostgreSQL"
 )
