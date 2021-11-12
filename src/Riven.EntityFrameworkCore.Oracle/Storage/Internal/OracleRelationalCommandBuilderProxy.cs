5ef0d68c92d730740e91178c9eb19464623934e0
staneee <yi.hang@live.com>
2021-11-12 16:30:06 +0800

    抽取常量

--- src/Riven.EntityFrameworkCore.Oracle/Storage/Internal/OracleRelationalCommandBuilderProxy.cs
+++ src/Riven.EntityFrameworkCore.Oracle/Storage/Internal/OracleRelationalCommandBuilderProxy.cs
@@ -4,16 +4,23 @@ namespace Microsoft.EntityFrameworkCore.Storage.Internal
 {
     public class OracleRelationalCommandBuilderProxy : DispatchProxy
     {
+        const string _doubleQuotation = "\"";
+        const string _methodName = nameof(IRelationalCommandBuilder.Append);
+
         IRelationalCommandBuilder _builder;
 
         protected override object Invoke(MethodInfo targetMethod, object[] args)
         {
-            if (targetMethod.Name == "Append" && args != null && args.Length == 1 && args[0] != null)
+            if (targetMethod.Name == _methodName
+                && args != null
+                && args.Length == 1
+                && args[0] != null
+                && args[0] is string)
             {
                 var val = args[0].ToString().Trim();
-                if (val.Contains("\""))
+                if (val.Contains(_doubleQuotation))
                 {
-                    args[0] = val.Replace("\"", string.Empty);
+                    args[0] = val.Replace(_doubleQuotation, string.Empty);
                 }
             }
 
