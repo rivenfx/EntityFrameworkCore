93c9cc62ab123197fe82ce48b9a22abee4ad22fb
staneee <yi.hang@live.com>
2021-11-12 16:26:37 +0800

    增加OracleRelationalCommandBuilderFactory实现

---
+++ src/Riven.EntityFrameworkCore.Oracle/Storage/Internal/OracleRelationalCommandBuilderProxy.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+namespace Microsoft.EntityFrameworkCore.Storage.Internal
+{
+    public class OracleRelationalCommandBuilderProxy : DispatchProxy
+    {
+        IRelationalCommandBuilder _builder;
+
+        protected override object Invoke(MethodInfo targetMethod, object[] args)
+        {
+            if (targetMethod.Name == "Append" && args != null && args.Length == 1 && args[0] != null)
+            {
+                var val = args[0].ToString().Trim();
+                if (val.Contains("\""))
+                {
+                    args[0] = val.Replace("\"", string.Empty);
+                }
+            }
+
+            return targetMethod.Invoke(this._builder, args);
+        }
+
+
+        public static IRelationalCommandBuilder Create(IRelationalCommandBuilder instance)
+        {
+            var obj = DispatchProxy.Create<IRelationalCommandBuilder, OracleRelationalCommandBuilderProxy>();
+            (obj as OracleRelationalCommandBuilderProxy)._builder = instance;
+
+            return obj;
+        }
+    }
+}
---
+++ src/Riven.EntityFrameworkCore.Oracle/Storage/Internal/RivenOracleRelationalCommandBuilderFactory.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+
+using Oracle.EntityFrameworkCore.Storage.Internal;
+
+namespace Microsoft.EntityFrameworkCore.Storage.Internal
+{
+    public class RivenOracleRelationalCommandBuilderFactory : OracleRelationalCommandBuilderFactory
+    {
+        public RivenOracleRelationalCommandBuilderFactory([NotNull] RelationalCommandBuilderDependencies dependencies)
+            : base(dependencies)
+        {
+        }
+
+        public override IRelationalCommandBuilder Create()
+        {
+            var instance = base.Create();
+            return OracleRelationalCommandBuilderProxy.Create(instance);
+        }
+    }
+}
