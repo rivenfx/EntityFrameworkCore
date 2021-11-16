4b200f400f9170edcf33e42f7aad807b146d9b49
staneee <yi.hang@live.com>
2021-11-16 10:17:46 +0800

    DevartOracle 增加 ModelBuilder Extenions

---
+++ src/Riven.EntityFrameworkCore.DevartOracle/Extensions/RivenDevartOracleModelBuilderExtenions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.EntityFrameworkCore.Extensions
+{
+    public static class RivenDevartOracleModelBuilderExtenions
+    {
+        /// <summary>
+        /// 表映射到 Devart Oracle, 表名/列名 大写
+        /// </summary>
+        /// <param name="modelBuilder">modelBuilder</param>
+        /// <param name="verifyingEntityType">验证实体类型是否需要处理</param>
+        /// <returns>modelBuilder</returns>
+        public static ModelBuilder TableMappingToDevartOracle(this ModelBuilder modelBuilder,
+            Func<IMutableEntityType, bool> verifyingEntityType)
+        {
+            return modelBuilder.TableMapping(verifyingEntityType, (s) => s.ToUpper());
+        }
+    }
+}
