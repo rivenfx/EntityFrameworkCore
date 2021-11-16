272ae154a85b845db69215938a7b2f18ec6e1b34
staneee <yi.hang@live.com>
2021-11-16 13:31:05 +0800

    优化实现

--- src/Riven.EntityFrameworkCore.Oracle/Storage/Internal/RivenOracleSqlGenerationHelper.cs
+++ src/Riven.EntityFrameworkCore.Oracle/Storage/Internal/RivenOracleSqlGenerationHelper.cs
@@ -1,5 +1,7 @@
 using JetBrains.Annotations;
 
+using Microsoft.EntityFrameworkCore.Extensions;
+
 using Oracle.EntityFrameworkCore.Storage.Internal;
 
 using System;
@@ -23,7 +25,7 @@ namespace Microsoft.EntityFrameworkCore.Storage.Internal
         public override string EscapeIdentifier(string identifier)
         {
             return base.EscapeIdentifier(
-                GenNewIdentifier(identifier)
+                RivenOracleModelBuilderExtenions.GenNewIdentifier(identifier)
             );
         }
 
@@ -33,57 +35,6 @@ namespace Microsoft.EntityFrameworkCore.Storage.Internal
                 builder,
                 identifier
             );
-        }
-
-        /// <summary>
-        /// 处理并生成新的 identifier
-        /// </summary>
-        /// <param name="identifier"></param>
-        /// <returns></returns>
-        public static string GenNewIdentifier(string identifier)
-        {
-            if (identifier.Length > 30)
-            {
-                // 主外键、索引超长
-                if (identifier.StartsWith("PK_")
-                    || identifier.StartsWith("FK_")
-                    || identifier.StartsWith("IX_"))
-                {
-                    if (!_identifierMap.TryGetValue(identifier, out string newIdentifier))
-                    {
-                        var start = identifier.Substring(0, 3);
-                        var end = GenerateMD5(identifier).Substring(0, 27);
-                        newIdentifier = $"{start}{end}";
-                        _identifierMap.Add(identifier, newIdentifier);
-                    }
-
-                    return newIdentifier.ToUpper();
-                }
-            }
-
-            return identifier;
-        }
-
-        /// <summary>
-        /// 字符串MD5
-        /// </summary>
-        /// <param name="txt"></param>
-        /// <returns></returns>
-        public static string GenerateMD5(string txt)
-        {
-            using (var mi = System.Security.Cryptography.MD5.Create())
-            {
-                byte[] buffer = Encoding.Default.GetBytes(txt);
-                //开始加密
-                byte[] newBuffer = mi.ComputeHash(buffer);
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < newBuffer.Length; i++)
-                {
-                    sb.Append(newBuffer[i].ToString("x2"));
-                }
-
-                return sb.ToString();
-            }
         }
     }
 }
