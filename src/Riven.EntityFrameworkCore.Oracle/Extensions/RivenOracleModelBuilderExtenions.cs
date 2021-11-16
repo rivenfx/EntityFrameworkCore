e7826913807831f28bdda67787fbfbf666cb3906
staneee <yi.hang@live.com>
2021-11-16 10:19:39 +0800

    Oracle增加ModelBuilderExtenions

---
+++ src/Riven.EntityFrameworkCore.Oracle/Extensions/RivenOracleModelBuilderExtenions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.EntityFrameworkCore.Extensions
+{
+    public static class RivenOracleModelBuilderExtenions
+    {
+        /// <summary>
+        /// 表映射到Oracle, 表名/列名 大写
+        /// </summary>
+        /// <param name="modelBuilder">modelBuilder</param>
+        /// <param name="verifyingEntityType">验证实体类型是否需要处理</param>
+        /// <returns>modelBuilder</returns>
+        public static ModelBuilder TableMappingToOracle(this ModelBuilder modelBuilder,
+            Func<IMutableEntityType, bool> verifyingEntityType)
+        {
+            return modelBuilder.TableMapping(verifyingEntityType, (s) => s.ToUpper());
+        }
+    }
+}
