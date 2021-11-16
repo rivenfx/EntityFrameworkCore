6dbf3856e9d7d3b61c0295b973ce87f81175d4f1
staneee <yi.hang@live.com>
2021-11-16 10:18:17 +0800

    PostgresSQL 增加ModelBuilderExtenions

---
+++ src/Riven.EntityFrameworkCore.PostgreSQL/Extensions/RivenPostgreSQLModelBuilderExtenions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.EntityFrameworkCore.Extensions
+{
+    public static class RivenPostgreSQLModelBuilderExtenions
+    {
+        /// <summary>
+        /// 表映射到 PostgreSQL, 表名/列名 小写
+        /// </summary>
+        /// <param name="modelBuilder">modelBuilder</param>
+        /// <param name="verifyingEntityType">验证实体类型是否需要处理</param>
+        /// <returns>modelBuilder</returns>
+        public static ModelBuilder TableMappingToPostgreSQL(this ModelBuilder modelBuilder,
+            Func<IMutableEntityType, bool> verifyingEntityType)
+        {
+            return modelBuilder.TableMapping(verifyingEntityType, (s) => s.ToLower());
+        }
+    }
+}
